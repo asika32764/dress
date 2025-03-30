425f0d3d19113afe9f8feea54b9f2ce30e8fb5aa
Simon Asika <asika32764@gmail.com>
2025-03-30 19:37:45 +0800

    misc

--- prepare-runpod.sh
+++ prepare-runpod.sh
@@ -1,9 +1,14 @@
-apt update
-apt install unzip
+#!/usr/bin/env bash
 
+apt update
+apt install unzip vim
+
+dir=$(pwd)
 python -m venv venv
-source venv/bin/activate
+source "$dir/venv/bin/activate"
 
 echo "export HF_HOME=/workspace/caches/huggingface\n" >> ~/.bashrc
 echo "export TRANSFORMERS_CACHE=/workspace/caches/transformers\n" >> ~/.bashrc
 echo "export TORCH_HOME=/workspace/caches/torch\n" >> ~/.bashrc
+
+source ~/.bashrc
