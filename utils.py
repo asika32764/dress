59ef4fe7fdeb5f7a9dcb758d0c6e260647bea2cd
Simon Asika <asika32764@gmail.com>
2025-03-30 16:47:07 +0800

    fix get device

--- utils.py
+++ utils.py
@@ -2,13 +2,13 @@
 
 import torch
 
-def get_device():
-    return torch.device("cpu")
-
 # def get_device():
-#     if torch.cuda.is_available():
-#         return torch.device("cuda")
-#     elif torch.backends.mps.is_available():
-#         return torch.device("mps")
-#     else:
-#         return torch.device("cpu")
+#     return torch.device("cpu")
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    # elif torch.backends.mps.is_available():
+    #     return torch.device("mps")
+    else:
+        return torch.device("cpu")
